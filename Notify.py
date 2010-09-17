a4a026877343de9f5b03d21fba89ca4436aa7113
Noel <Noel.Dawe@cern.ch>
2010-09-16 17:24:09 -0700

    updates

--- Notify.py
+++ Notify.py
@@ -1,25 +1,13 @@
-import os
 import subprocess
 
-class Reaper:
+def send(hosts,title,message,icon="python"):
 
-
-
-class Notify:
-
-    def __init__(self):
-
-    def __del__(self):
-
-
-    def send(hosts,title,message,icon="python"):
-
-        if type(hosts) is not list:
-            hosts = [hosts]
-        
-        cmd = "ssh -X %s 'notify-send -i %s \"%s\" \"%s\"'"
-        children = []
-        for host in hosts:
-            children.append(subprocess.Popen(args=cmd%(icon,host,title,message),shell=True))
-        for child in children:
-            child.wait()
+    if type(hosts) is not list:
+        hosts = [hosts]
+    
+    cmd = "ssh -X %s 'notify-send -i %s \"%s\" \"%s\"'"
+    children = []
+    for host in hosts:
+        children.append(subprocess.Popen(args=cmd%(host,icon,title,message),shell=True))
+    for child in children:
+        child.wait()
