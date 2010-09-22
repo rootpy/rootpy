10743b3cf805300d968a1af2d44963831e5cb4a8
Noel Dawe <noel.dawe@gmail.com>
2010-09-21 19:28:12 -0700

    adding documentation

---
+++ doc/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({desctypes:{},terms:{index:0,search:0,welcom:0,rootpi:0,modul:0,indic:0,content:0,tabl:0,document:0,page:0},titles:["Welcome to ROOTPy&#8217;s documentation!"],modules:{},descrefs:{},filenames:["index"]})
