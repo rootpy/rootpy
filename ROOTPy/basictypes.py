d4c4db630f330d249d255ded111ee3613d768096
Noel <Noel.Dawe@cern.ch>
2010-11-08 18:32:35 -0800

    updates

--- ROOTPy/basictypes.py
+++ ROOTPy/basictypes.py
@@ -1,10 +1,10 @@
+# -*- coding: iso-8859-15 -*-
 from array import array
 
 class Variable(array):
     """This is the base class for all variables"""        
     def __init__(self):
 
-        self.typename = float
         self.default = 0
         
     def reset(self):
@@ -21,7 +21,9 @@ class Variable(array):
 
     def set(self,value):
         """Set the value"""
-        self[0] = self.typename(value)
+        self[0] = self._convert(value)
+
+    def _convert(self,value): pass
     
     def __str__(self):
         
@@ -41,33 +43,70 @@ class Variable(array):
 
     def __lt__(self,value):
 
+        if isinstance(value,Variable):
+            return self[0] < value[0]
         return self[0] < value
 
     def __le__(self,value):
 
+        if isinstance(value,Variable):
+            return self[0] <= value[0]
         return self[0] <= value
     
     def __eq__(self,value):
-
+        
+        if isinstance(value,Variable):
+            return self[0] == value[0]
         return self[0] == value
 
     def __ne__(self,value):
-
+        
+        if isinstance(value,Variable):
+            return self[0] != value[0]
         return self[0] != value
 
     def __gt__(self,value):
-
+        
+        if isinstance(value,Variable):
+            return self[0] > value[0]
         return self[0] > value
     
     def __ge__(self,value):
-
+        
+        if isinstance(value,Variable):
+            return self[0] >= value[0]
         return self[0] >= value
 
     def __nonzero__(self):
 
         return self[0] != 0
+    
+    def __add__(self,other):
+
+        if isinstance(other,Variable):
+            return self[0] + other[0]
+        return self[0] + other
+    
+    def __sub__(self,other):
+
+        if isinstance(other,Variable):
+            return self[0] - other[0]
+        return self[0] - other
+
+    def __mul__(self,other):
+
+        if isinstance(other,Variable):
+            return self[0] * other[0]
+        return self[0] * other
+
+    def __div__(self,other):
+
+        if isinstance(other,Variable):
+            return self[0] / other[0]
+        return self[0] / other
 
 """
+ROOT:
 - C : a character string terminated by the 0 character
 - B : an 8 bit signed integer (Char_t)
 - b : an 8 bit unsigned integer (UChar_t)
@@ -79,8 +118,49 @@ class Variable(array):
 - D : a 64 bit floating point (Double_t)
 - L : a 64 bit signed integer (Long64_t)
 - l : a 64 bit unsigned integer (ULong64_t)
-- O : a boolean (Bool_t)
+- O : a boolean (Bool_t) (see note 1)
+
+Python array:
+Code    C Type          Python Type     Minimum Size in Bytes
+'c'     char            character           1
+'b'     signed char     int                 1
+'B'     unsigned char   int                 1
+'u'     Py_UNICODE      Unicode character   2 (see note 2)
+'h'     signed short    int                 2
+'H'     unsigned short  int                 2
+'i'     signed int      int                 2
+'I'     unsigned int    long                2
+'l'     signed long     int                 4
+'L'     unsigned long   long                4
+'f'     float           float               4
+'d'     double          float               8
+
+Note 1: Use an unsigned type for array.array to represent a Bool_t
+Note 2: The 'u' typecode corresponds to Python’s unicode character. On narrow Unicode builds this is 2-bytes, on wide builds this is 4-bytes.
 """
+
+#________________________________________________________________________
+
+class Bool(Variable):
+    """This is a variable containing a Boolean type"""
+    def __new__(cls, default=False):
+        
+        if default < 0:
+            default = 0
+        return Variable.__new__(cls,'B',[int(bool(default))])
+
+    def __init__(self, default=False):
+
+        Variable.__init__(self)
+        self.default = self._convert(default)
+
+    def _convert(self, value):
+
+        return int(bool(value))
+    
+    def type(self):
+        """The ROOT character representation of the Boolean type"""
+        return 'O'
 
 #________________________________________________________________________
 
@@ -95,11 +175,14 @@ class Int(Variable):
     def __init__(self, default=0):
 
         Variable.__init__(self)
-        self.typename = int
         self.default = int(default)
+   
+    def _convert(self, value):
     
+        return int(value)
+         
     def type(self):
-        """The character representation of the integer type"""
+        """The ROOT character representation of the integer type"""
         return 'I'
 
 #________________________________________________________________________
@@ -115,13 +198,14 @@ class UInt(Variable):
     def __init__(self, default=0):
 
         Variable.__init__(self)
-        if default < 0:
-            default = 0
-        self.typename = long
-        self.default = long(default)
+        self.default = self._convert(default)
+
+    def _convert(self, value):
+
+        return long(abs(value))
     
     def type(self):
-        """The character representation of the unsigned integer type"""
+        """The ROOT character representation of the unsigned integer type"""
         return 'i'
 
 #__________________________________________________________________________
@@ -135,9 +219,12 @@ class Float(Variable):
     def __init__(self, default=0.):
         
         Variable.__init__(self)
-        self.typename = float
         self.default = float(default)
+   
+    def _convert(self, value):
+
+        return float(value)
     
     def type(self):
-        """The character representation of the float type"""
+        """The ROOT character representation of the float type"""
         return 'F'
