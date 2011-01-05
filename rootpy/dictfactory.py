bf787bc0564e19fb575c2706dbddc273f53955a0
Noel Dawe <Noel.Dawe@cern.ch>
2011-01-05 02:45:44 -0600

    updates

--- rootpy/dictfactory.py
+++ rootpy/dictfactory.py
@@ -10,20 +10,18 @@ import atexit
 import uuid
 from rootpy.userdata import DATA_ROOT
 
-__ROOT_version = ROOT.gROOT.GetVersionCode()
-
+__ROOT_version = str(ROOT.gROOT.GetVersionCode())
 __loaded_dicts = {}
-
-__dicts_path = os.path.join(DATA_ROOT, 'dicts', str(__ROOT_version))
+__dicts_path = os.path.join(DATA_ROOT, 'dicts')
+__lookup_table_name = 'lookup'
 
 if not os.path.exists(__dicts_path):
     os.makedirs(__dicts_path)
 
-__lookup_table_name = 'lookup'
-
 if os.path.exists(os.path.join(__dicts_path, __lookup_table_name)):
     __lookup_file = open(os.path.join(__dicts_path, __lookup_table_name), 'r')
-    __lookup_table = dict([reversed(line.strip().split('\t')) for line in __lookup_file.readlines()])
+    __lines = [reversed(line.strip().split('\t')) for line in __lookup_file.readlines()]
+    __lookup_table = dict([(a, (b,c)) for a,b,c in __lines])
     __lookup_file.close()
 else:
     __lookup_table = {}
@@ -42,10 +40,11 @@ def generate(declaration, headers = None):
     
     # If as .so already exists for this class, use it.
     if __lookup_table.has_key(unique_name):
-        if ROOT.gSystem.Load(__lookup_table[unique_name]+"_C.so") == 0:
-            __loaded_dicts[unique_name] = None
-            return True
-        return False
+        if __ROOT_version == __lookup_table[unique_name][0]:
+            if ROOT.gSystem.Load(__lookup_table[unique_name][1]+"_C.so") == 0:
+                __loaded_dicts[unique_name] = None
+                return True
+            return False
     
     # This dict was not previously generated so we must create it now
     source = ""
@@ -72,7 +71,7 @@ def generate(declaration, headers = None):
     success = ROOT.gROOT.ProcessLine(".L %s+"% sourcefilename) == 0
     ROOT.gErrorIgnoreLevel = msg_level
     if success:
-        __lookup_table[unique_name] = dict_id
+        __lookup_table[unique_name] = (__ROOT_version, dict_id)
         __loaded_dicts[unique_name] = None
     else:
         os.unlink(sourcefilename)
@@ -83,6 +82,6 @@ def generate(declaration, headers = None):
 @atexit.register
 def __cleanup():
     __lookup_file = open(os.path.join(__dicts_path, __lookup_table_name), 'w')
-    for name, dict_id in __lookup_table.items():
-        __lookup_file.write("%s\t%s\n"% (dict_id, name))
+    for name, (version, dict_id) in __lookup_table.items():
+        __lookup_file.write("%s\t%s\t%s\n"% (dict_id, version, name))
     __lookup_file.close()
