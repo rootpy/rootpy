272818b917ab4790dce2caf8334af3a971a96aa1
Noel Dawe <Noel.Dawe@cern.ch>
2011-01-08 21:11:47 -0800

    updates

--- rootpy/filtering.py
+++ rootpy/filtering.py
@@ -1,19 +1,10 @@
 
 class Filter(object):
 
-    def __init__(self,buffer,verbose=False):
+    def __init__(self):
         
-        self.buffer = buffer
-        self.verbose = verbose
         self.total = 0
         self.passing = 0
-
-    def __getstate__(self):
-
-        return {"buffer":None,
-                "verbose":False,
-                "total":self.total,
-                "passing":self.passing}
     
     def __str__(self):
 
@@ -25,26 +16,25 @@ class Filter(object):
                "Total: %i\n"%(self.total)+\
                "Pass:  %i"%(self.passing)
 
-    def __add__(self,other):
+    def __add__(self, other):
 
-        newfilter = self.__class__(self.buffer,self.verbose)
+        newfilter = self.__class__()
         newfilter.total = self.total + other.total
         newfilter.passing = self.passing + other.passing
         return newfilter
     
-    def __nonzero__(self):
+    def __call__(self, event):
 
-        if self.verbose: print "processing filter %s..."%(self.__class__.__name__)
         self.total += 1
         if self.passes():
             self.passing += 1
             return True
         return False
-    
-    def passes(self): pass
 
 class FilterList(list):
 
-    def __nonzero__(self):
+    def __call__(self, event):
 
-        return all(self)
+        for filter in self:
+            if not filter(event):
+                return False
