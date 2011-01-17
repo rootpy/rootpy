e0e29e602196c6babc6ad1af75748edede29176c
end <end@4525493e-7705-40b1-a816-d608a930855b>
2011-01-17 10:45:47 +0000

    updates
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasusr/end/rootpy/trunk@26457 4525493e-7705-40b1-a816-d608a930855b

--- rootpy/utils.py
+++ rootpy/utils.py
@@ -14,23 +14,6 @@ def asrootpy(tobject):
     tobject.__class__ = cls
     for init in inits:
         init(tobject)
-
-    """
-    if isinstance(tobject, ROOT.TTree):
-        tobject.__class__ = Ntuple
-    elif isinstance(tobject, ROOT.TH1):
-        tobject.__class__ = plotting._Hist_class(rootclass = tobject.__class__)
-        tobject._post_init()
-    elif isinstance(tobject, ROOT.TH2):
-        tobject.__class__ = plotting._Hist2D_class(rootclass = tobject.__class__)
-        tobject._post_init()
-    elif isinstance(tobject, ROOT.TH3):
-        tobject.__class__ = plotting._Hist3D_class(rootclass = tobject.__class__)
-        tobject._post_init()
-    elif isinstance(tobject, ROOT.TGraphAsymmErrors):
-        tobject.__class__ = plotting.Graph
-    """
-    
     
     if isinstance(tobject, Plottable):
         tobject.decorate(template)
