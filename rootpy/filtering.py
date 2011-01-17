0b079ff0a5f9f23e5b3b0e8ab309573b51be9cf8
end <end@4525493e-7705-40b1-a816-d608a930855b>
2011-01-17 10:51:38 +0000

    updates
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlasusr/end/rootpy/trunk@26494 4525493e-7705-40b1-a816-d608a930855b

--- rootpy/filtering.py
+++ rootpy/filtering.py
@@ -1,6 +1,15 @@
-
+"""
+This module defines a framework for filtering Trees.
+The user must write a class which inherits from Filter and
+"""
 class Filter(object):
-
+    """
+    The base class from which all filter classes must inherit from.
+    The derived class must override the passes method which returns True
+    if ths event passes and returns False if not.
+    The number of passing and failing events are recorded and may be used
+    later to create a cut-flow.
+    """
     def __init__(self):
         
         self.total = 0
@@ -31,8 +40,14 @@ class Filter(object):
             return True
         return False
 
-class FilterList(list):
+    def passes(self, event):
 
+        raise NotImplementedError("You must override this method in your derived class")
+
+class FilterList(list):
+    """
+    Creates a list of Filters for convenient evaluation of a sequence of Filters.
+    """
     def __call__(self, event):
 
         for filter in self:
