6373ec749d5de542827359820918e178ced956e0
Noel Dawe <Noel.Dawe@cern.ch>
2011-01-26 12:11:50 -0800

    updates

---
+++ rootpy/branchselect.py
@@ -0,0 +1,14 @@
+import inspect
+import re
+
+class BranchCollection(list):
+
+    def parse(thing, treenames): pass
+
+        src = inspect.getsource(thing)
+        if type(treenames) is not list:
+            treenames = [treenames]
+        for name in treenames:
+            pattern = "%s.%s"% (name, "(?P<branch>\w+)")
+            for match in re.finditer(pattern, src):
+                self.append(match.group('branch'))
