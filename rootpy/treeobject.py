e0eb41432a8bcde61d5711afde2c94113ca89e5a
Noel Dawe <Noel.Dawe@cern.ch>
2011-01-26 22:01:31 -0800

    updates

--- rootpy/treeobject.py
+++ rootpy/treeobject.py
@@ -3,3 +3,10 @@ from collections import namedtuple
 def treeobject(name, tree, prefix):
 
     class name: pass
+
+class TreeObjectCollection(list):
+
+    def __init__(self, name, tree, prefix):
+
+        self.name = name
+        self.tree = tree
