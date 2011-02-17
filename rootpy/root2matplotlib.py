1dd0ddbbe9bdeddb79964126c15e3aedf1a771fe
Noel Dawe <Noel.Dawe@cern.ch>
2011-02-17 23:26:59 +0100

    updates

--- rootpy/root2matplotlib.py
+++ rootpy/root2matplotlib.py
@@ -4,13 +4,21 @@ import matplotlib.pyplot as plt
 def hist(h, **kwargs):
 
     fillstyle = h.GetFillStyle()
-    return plt.hist(h.xcenters, weights = h, bins = h.xedges,
-        facecolor = h.GetFillColor(),
-        edgecolor = h.GetLineColor(),
-        fill = (fillstyle != "hollow"),
-        hatch = (fillstyle if fillstyle not in ["", "hollow", "solid"] else None),
-        linestyle = h.GetLineStyle(),
-        linewidth = h.GetLineWidth(),
-        label = h.GetTitle(),
-        visible = h.visible,
-        **kwargs)
+    if not kwargs.has_key('facecolor'):
+        kwargs['facecolor'] = h.GetFillColor()
+    if not kwargs.has_key('edgecolor'):
+        kwargs['edgecolor'] = h.GetLineColor()
+    if not kwargs.has_key('fill'):
+        kwargs['fill'] = (fillstyle != "hollow")
+    if not kwargs.has_key('hatch'):
+        kwargs['hatch'] = (fillstyle if fillstyle not in ["", "hollow", "solid"] else None)
+    if not kwargs.has_key('linestyle'):
+        kwargs['linestyle'] = h.GetLineStyle()
+    if not kwargs.has_key('linewidth'):
+        kwargs['linewidth'] = h.GetLineWidth()
+    if not kwargs.has_key('label'):
+        kwargs['label'] = h.GetTitle()
+    if not kwargs.has_key('visible'):
+        kwargs['visible'] = h.visible
+
+    return plt.hist(h.xcenters, weights = h, bins = h.xedges, **kwargs)
