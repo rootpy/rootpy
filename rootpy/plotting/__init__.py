9f220a395286f131f420794908174c1a4b4e8512
Noel Dawe <Noel.Dawe@cern.ch>
2011-06-27 00:19:01 -0700

    updates

--- rootpy/plotting/__init__.py
+++ rootpy/plotting/__init__.py
@@ -1,4 +1,8 @@
-from .hist import Hist, Hist2D, Hist3D, Efficiency
+from .hist import Hist, Hist2D, Hist3D
+# Exists only in ROOT >=5.28
+try:
+    from .hist import Efficiency
+except: pass
 from .graph import Graph, Graph2D
 from .legend import Legend
 from .canvas import Canvas, Pad
