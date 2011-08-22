3d56c966043182542c93c2485f590f7d041e3980
Jeff Klukas <klukas@wisc.edu>
2011-08-22 11:36:01 -0500

    Added walk utility and added Directory class

--- rootpy/io/file.py
+++ rootpy/io/file.py
@@ -3,20 +3,44 @@ This module enhances IO-related ROOT funcionality
 """
 import ROOT
 from ..utils import asrootpy
+from . import utils
 
-class File(ROOT.TFile):
+class Directory(ROOT.TDirectory):
     """
-    Inherits from TFile
+    Inherits from TDirectory
     """
     def __init__(self, *args, **kwargs):
 
-        ROOT.TFile.__init__(self, *args)
+        ROOT.TDirectory.__init__(self, *args)
 
     def Get(self, name):
         """
         Attempt to convert requested object into rootpy form
         """
-        return asrootpy(ROOT.TFile.Get(self, name))
+        return asrootpy(ROOT.TDirectory.Get(self, name))
+
+    def GetDirectory(self, name):
+        """
+        Should return a Directory object rather than TDirectory
+        """
+        #TODO: how to get asrootpy to return a Directory object?
+        return asrootpy(ROOT.TDirectory.GetDirectory(self, name))
+
+    def walk(self, top=None):
+        """
+        Calls :func:`rootpy.io.utils.walk`.
+        """
+        return utils.walk(self, top)
+
+
+class File(ROOT.TFile, Directory):
+    """
+    Inherits from Directory
+    """
+    
+    def __init__(self, *args, **kwargs):
+
+        ROOT.TFile.__init__(self, *args)
 
 def openFile(filename, mode=""):
 
--- rootpy/io/utils.py
+++ rootpy/io/utils.py
@@ -1,5 +1,36 @@
 """
-This module should contain os.path/walk-like utilities for the ROOT TFile "filesystem"
+This module should contain os.path/walk-like utilities for the ROOT TFile 'filesystem'
 """
 
-def walk(): pass
+from os.path import join
+
+def walk(tdirectory, top=None):
+    """
+    For each directory in the directory tree rooted at top (including top
+    itself, but excluding '.' and '..'), yields a 3-tuple
+
+    dirpath, dirnames, filenames
+
+    dirpath is a string, the path to the directory.  dirnames is a list of
+    the names of the subdirectories in dirpath (excluding '.' and '..').
+    filenames is a list of the names of the non-directory files in dirpath.
+    Note that the names in the lists are just names, with no path components.
+    To get a full path (which begins with top) to a file or directory in
+    dirpath, do os.path.join(dirpath, name).
+    """
+    dirnames, objectnames = [], []
+    if top:
+        tdirectory = tdirectory.GetDirectory(top)
+    keys = tdirectory.GetListOfKeys()
+    for key in keys:
+        name = key.GetName()
+        classname = key.GetClassName()
+        # print name, classname
+        if 'TDirectory' in classname:
+            dirnames.append(name)
+        else:
+            objectnames.append(name)
+    yield tdirectory, dirnames, objectnames
+    for dirname in dirnames:
+        for x in walk(tdirectory.GetDirectory(dirname)):
+            yield x
