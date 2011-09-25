b8390e079b2c50eb937a5940b13327919ccde015
Noel Dawe <Noel.Dawe@cern.ch>
2011-09-24 18:48:24 -0700

    updates

--- rootpy/info.py
+++ rootpy/info.py
@@ -1,4 +1,4 @@
 __VERSION__ = '0.5'
 __URL__ = 'http://ndawe.github.com/rootpy'
 __GITURL__ = 'https://github.com/ndawe/rootpy/'
-__DOWNLOAD_URL__ = __GITURL__+'tarball/master'
+__DOWNLOAD_URL__ = 'https://github.com/downloads/ndawe/rootpy/rootpy-%s.tar.gz' % __VERSION__
