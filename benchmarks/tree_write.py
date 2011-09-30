558c7a07ed5658d9d0f8d01fdfabd4ca64da2b5a
Noel <Noel.Dawe@cern.ch>
2011-09-29 17:09:24 -0700

    adding benchmarks

---
+++ benchmarks/tree_write.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+from rootpy.tree import Tree, TreeModel
+from rootpy.io import open
+from rootpy.types import *
+from random import gauss, randint
+import ROOT
+
+f = open("test.root", "recreate")
+
+
+# define the model
+class Event(TreeModel):
+
+    x = FloatCol()
+    y = FloatCol()
+    z = FloatCol()
+
+    a = ROOT.vector("float")
+    b = ROOT.vector("float")
+    c = ROOT.vector("float")
+
+    d = ROOT.vector("vector<float>")
+    e = ROOT.vector("vector<float>")
+    f = ROOT.vector("vector<float>")
+
+tree = Tree("test", model=Event)
+
+# fill the tree
+for i in xrange(500000):
+    tree.x = gauss(.5, 1.)
+    tree.y = gauss(.3, 2.)
+    tree.z = gauss(13., 42.)
+    
+    for i in xrange(randint(1,10)):
+        tree.a.push_back(gauss(.5, 1.))
+    for i in xrange(randint(1,10)):
+        tree.b.push_back(gauss(.5, 1.))
+    for i in xrange(randint(1,10)):
+        tree.c.push_back(gauss(.5, 1.))
+
+    for i in xrange(randint(1,10)):
+        t = ROOT.vector("float")()
+        for j in xrange(randint(1,10)):
+            t.push_back(gauss(.5, 1.))
+        tree.d.push_back(t)
+    for i in xrange(randint(1,10)):
+        t = ROOT.vector("float")()
+        for j in xrange(randint(1,10)):
+            t.push_back(gauss(.5, 1.))
+        tree.e.push_back(t)
+    for i in xrange(randint(1,10)):
+        t = ROOT.vector("float")()
+        for j in xrange(randint(1,10)):
+            t.push_back(gauss(.5, 1.))
+        tree.f.push_back(t)
+
+    tree.fill(reset=True)
+tree.write()
+
+f.close()
