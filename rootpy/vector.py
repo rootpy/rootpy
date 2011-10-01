186e29f1065284c349ecc0499d2993db0f9cf36e
Noel Dawe <Noel.Dawe@cern.ch>
2011-09-30 17:09:20 -0700

    better arithmetic operators

--- rootpy/vector.py
+++ rootpy/vector.py
@@ -1,48 +1,81 @@
-from .core import _repr_mixin, camelCaseMethods
+from .core import _repr_mixin, camelCaseMethods, isbasictype
 from ROOT import TLorentzVector, TVector3, TVector2
 from copy import copy
 
 
 class _arithmetic_mixin(object):
 
+    def __mul__(self, other):
+       
+        prod = self.__class__.__bases__[-1].__mul__(self, other)
+        if isinstance(prod, self.__class__.__bases__[-1]):
+            prod.__class__ = self.__class__
+        return prod
+
+    def __imul__(self, other):
+
+        if isinstance(other, self.__class__):
+            raise TypeError("Attemping to set vector to scalar quantity")
+        try:
+            prod = self.__class__.__bases__[-1].__mul__(self, other)
+        except TypeError:
+            raise TypeError("Invalid operation")
+        prod.__class__ = self.__class__
+        self = prod
+        return self
+
+    def __rmul__(self, other):
+        
+        return self * other
+    
     def __add__(self, other):
         
-        clone = copy(self)
-        if other:
-            clone = self.__class__.__bases__[-1].__add__(clone, other)
-            clone.__class__ = self.__class__
+        try:
+            clone = self.__class__.__bases__[-1].__add__(self, other)
+        except TypeError:
+            raise TypeError("Invalid operation")
+        clone.__class__ = self.__class__
         return clone
-    
-    def __radd__(self, other):
 
-        clone = copy(self)
-        if other:
-            return self + other
-        return clone
+    def __radd__(self, other):
+        
+        if other == 0:
+            return copy(self)
+        raise TypeError("Invalid operation")
     
     def __iadd__(self, other):
         
-        self.__class__.__bases__[-1].__add__(self, other)
+        try:
+            _sum = self.__class__.__bases__[-1].__add__(self, other)
+        except TypeError:
+            raise TypeError("Invalid operation")
+        _sum.__class__ = self.__class__
+        self = _sum
         return self
-        
+
     def __sub__(self, other):
-
-        clone = copy(self)
-        if other:
-            clone = self.__class__.__bases__[-1].__sub__(clone, other)
-            clone.__class__ = self.__class__
+        
+        try:
+            clone = self.__class__.__bases__[-1].__sub__(self, other)
+        except TypeError:
+            raise TypeError("Invalid operation")
+        clone.__class__ = self.__class__
         return clone
-    
+
     def __rsub__(self, other):
-
-        clone = copy(self)
-        if other:
-            raise AttributeError
-        return clone
-    
+        
+        if other == 0:
+            return copy(self)
+        raise TypeError("Invalid operation")
+         
     def __isub__(self, other):
     
-        self.__class__.__bases__[-1].__sub__(self, other)
+        try:
+            diff = self.__class__.__bases__[-1].__sub__(self, other)
+        except TypeError:
+            raise TypeError("Invalid operation")
+        diff.__class__ = self.__class__
+        self = diff
         return self
 
     def __copy__(self):
