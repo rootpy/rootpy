c1a460d1e6a1758b925e1dac879d11f0505b11ca
Noel Dawe <Noel.Dawe@cern.ch>
2011-10-16 20:15:57 -0700

    fix

--- rootpy/root2array.py
+++ rootpy/root2array.py
@@ -6,17 +6,20 @@ This module should handle:
 from .types import Variable, convert
 import numpy as np
 
-def to_numpy_array(trees, branches=None, use_cache=False, cache_size=1000000):
+def to_numpy_array(trees, branches=None,
+                   use_cache=False, cache_size=1000000,
+                   include_weight=False,
+                   weight_dtype='f4'):
     
     if type(trees) not in (list, tuple):
         trees = [trees]
     # if branches is None then select only branches with basic types
     # i.e. no vectors or other special objects
-    _branches = []
+    _branches = {}
     if branches is None:
         for name, value in tree.buffer.items():
             if isinstance(value, Variable):
-                _branches.append((name, value))
+                _branches[name] = value
     else:
         for branch in branches:
             if branch not in tree.buffer:
@@ -25,10 +28,15 @@ def to_numpy_array(trees, branches=None, use_cache=False, cache_size=1000000):
             if not isinstance(value, Variable):
                 raise TypeError("Branch %s is not a basic type: %s" % 
                                 (branch, type(value)))
-            _branches.append((branch, tree.buffer[branch]))    
+            _branches[branch] = tree.buffer[branch]
     if not _branches:
         return None
     dtype = [(name, convert('ROOTCODE', 'NUMPY', value.type)) for name, value in _branches]
+    if include_weight:
+        if 'weight' not in _branches.keys():
+            dtype.append(('weight', weight_dtype))
+        else:
+            include_weight = False
     total_entries = sum([tree.GetEntries() for tree in trees])
     array = np.recarray(shape=(total_entries,), dtype=dtype)
     i = 0
@@ -36,8 +44,11 @@ def to_numpy_array(trees, branches=None, use_cache=False, cache_size=1000000):
         tree.use_cache(use_cache, cache_size=cache_size, learn_entries=1)
         if use_cache:
             tree.always_read([name for name, value in _branches])
+        weight = tree.GetWeight()
         for entry in tree:
             for j, (branch, value) in enumerate(_branches):
                 array[i][j] = entry[branch].value
-                i += 1
+            if include_weight:
+                array[i][-1] = weight
+            i += 1
     return array
