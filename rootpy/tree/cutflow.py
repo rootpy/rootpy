05fbf2a4968174de4b43449c370dc32355c9fc29
Noel Dawe <Noel.Dawe@cern.ch>
2011-10-22 14:16:45 -0700

    updates

--- rootpy/tree/cutflow.py
+++ rootpy/tree/cutflow.py
@@ -5,12 +5,30 @@ class Cutflow(object):
     def __init__(self, names):
     
         self.__names = names
-        self.__dict = dict((name, '0') for name in names)
+        self.__dict = None
+        self.reset()
 
-    def __setitem__(self, item, value):
+    def __setitem__(self, name, passes):
 
-        self.__dict[item] = str(int(bool(value)))
+        self.__dict[name] = str(int(bool(passes)))
     
+    def passes(self, name):
+        
+        self.__dict[name] = '1'
+    
+    def stages(self):
+        
+        self.reset()
+        yield self
+        for name in self.__names:
+           self.passes(name)
+           yield self
+        self.reset()
+
+    def reset(self):
+
+        self.__dict = dict((name, '0') for name in self.__names)
+
     def bitstring(self):
 
         return ''.join([self.__dict[item] for item in self.__names])
