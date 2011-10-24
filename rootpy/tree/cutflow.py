2429771ea699c3fe0579bc9925f698024fa20d25
Noel Dawe <Noel.Dawe@cern.ch>
2011-10-24 09:00:28 -0700

    updates

--- rootpy/tree/cutflow.py
+++ rootpy/tree/cutflow.py
@@ -45,3 +45,41 @@ class Cutflow(object):
         if not self.__dict:
             return 0
         return int(self.bitstring(), 2)
+
+
+from tabulartext import TextTable
+
+
+class CutflowTable(object):
+
+    def __init__(self, lumi = 1.):
+
+        self.lumi = lumi
+        self.samples = []
+        self.cut_titles = []
+
+    def add_sample(sample, name, weight = 1.):
+
+        titles = [cut[0] for cut in sample]
+        if not self.samples:
+            self.cut_titles = titles
+        elif titles != self.cut_titles:
+            raise ValueError("mismatching cut-flows: names don't match") 
+        self.samples.append((weight, name, [cut[1] for cut in sample]))
+
+    def __str__(self):
+
+        return self.__repr__()
+
+    def __repr__(self):
+
+        if not self.samples:
+            return ''
+        table = TextTable()
+        table.set_deco(TextTable.HEADER)
+        table.add_row(['']+[name for weight, name, cuts in self.samples])
+        for i, title in enumerate(self.cut_titles):
+            table.add_row([self.cut_titles[i]] + 
+                          [weight * cuts * self.lumi for \
+                           weight, name, cuts in self.samples]) 
+        return table.draw()
