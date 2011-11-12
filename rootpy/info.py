702f2ea6e2d58ba7e291b863055839db711f3535
Noel Dawe <Noel.Dawe@cern.ch>
2011-11-13 00:01:10 +0100

    increment version number

--- rootpy/info.py
+++ rootpy/info.py
@@ -1,4 +1,4 @@
-__VERSION__ = '0.5'
+__VERSION__ = '0.6'
 __URL__ = 'http://ndawe.github.com/rootpy'
 __GITURL__ = 'https://github.com/ndawe/rootpy/'
 __DOWNLOAD_URL__ = 'https://github.com/downloads/ndawe/rootpy/rootpy-%s.tar.gz' % __VERSION__
