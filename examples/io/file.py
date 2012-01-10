c56bcc25ba5b99575a428cae50f2f731ac4ff17a
Noel Dawe <Noel.Dawe@cern.ch>
2012-01-10 12:05:30 -0800

    more details in file example

--- examples/io/file.py
+++ examples/io/file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 from rootpy.io import open, DoesNotExist
+from rootpy.plotting import Hist, Hist2D
+from rootpy.utils import asrootpy
 
 f = open('data.root')
 
@@ -18,5 +20,36 @@ for thing in f.walk():
 f.close()
 
 # supports with statements
-with open('data.root') as f:
+with open('data.root', 'update') as f:
     print f
+
+    # write some histograms
+    h1 = Hist(100, 0, 100, name='h1')
+    # variable bin widths
+    h2 = Hist2D((0,3,5,20,50), (10,20,30,40,1000), name='h2')
+
+    h1.Write()
+    h2.Write()
+# file is automatically closed after with statement
+
+# retrieve the histograms previously saved
+with open('data.root') as f:
+
+    h1 = f.h1
+    # or h1 = f.Get('h1')
+    h2 = f.h2
+    # or h2 = f.Get('h2')
+
+    # ROOT classes are automatically converted into
+    # rootpy form when retrieved from a ROOT file as
+    # long as their module was previously imported
+    print h1.__class__.__name__
+    print h2.__class__.__name__
+
+    # you may also do this to convert an object into
+    # rootpy form (again, assuming the relevant module
+    # was previously imported)
+    h1 = asrootpy(h1)
+    # if it is already in rootpy form or if not rootpy form
+    # exists then asrootpy does nothing
+    print h1.__class__.__name__
