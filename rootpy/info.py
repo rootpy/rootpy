f930d5943641fb1fa447858532df398c394edfee
Noel Dawe <Noel.Dawe@cern.ch>
2012-04-11 17:13:26 -0700

    better versioning

--- rootpy/info.py
+++ rootpy/info.py
@@ -8,9 +8,38 @@
       %s
 """
 
-__version_info__ = (0, 6)
-__version__ = '.'.join(map(str, __version_info__))
+from collections import namedtuple
+
+
+_version_info_base = namedtuple('version_info',
+                                ['major',
+                                 'minor',
+                                 'micro',
+                                 'releaselevel',
+                                 'serial'])
+
+
+class version_info(_version_info_base):
+
+    def __repr__(self):
+
+        return 'rootpy.%s' % super(version_info, self).__repr__()
+
+    def __str__(self):
+
+        if self.releaselevel != 'final':
+            return '%s.%s.%s-%s%s' % (self.major,
+                                      self.minor,
+                                      self.micro,
+                                      self.releaselevel[0],
+                                      self.serial)
+        return '%s.%s.%s' % (self[:3])
+
+
+__version_info__ = version_info(0, 6, 0, 'alpha', 0)
+__version__ = str(__version_info__)
 __url__ = 'http://rootpy.github.com/rootpy'
 __repo_url__ = 'https://github.com/rootpy/rootpy/'
-__download_url__ = 'http://pypi.python.org/packages/source/r/rootpy/rootpy-%s.tar.gz' % __version__
+__download_url__ = ('http://pypi.python.org/packages/source/r/'
+                    'rootpy/rootpy-%s.tar.gz') % __version__
 __doc__ %= __version__
