da4de6f4da893214d70f94a6234efb4077c365ae
Noel <Noel.Dawe@cern.ch>
2012-05-09 14:36:29 -0700

    fix examples

--- examples/plotting/matplotlib_histstack_example.py
+++ examples/plotting/matplotlib_histstack_example.py
@@ -4,6 +4,11 @@ from rootpy.plotting import Hist, HistStack
 import rootpy.plotting.root2matplotlib as rplt
 import matplotlib.pyplot as plt
 
+import ROOT
+# Setting this to True (default in rootpy)
+# changes how the histograms look in ROOT...
+ROOT.TH1.SetDefaultSumw2(False)
+
 # create normal distributions
 mu1, mu2, sigma = 100, 140, 15
 x1 = mu1 + sigma * np.random.randn(10000)
