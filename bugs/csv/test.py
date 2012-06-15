9a7e15a26af8597c2a14790abc83f5f5d942c64d
Noel Dawe <Noel.Dawe@cern.ch>
2012-06-14 19:52:12 -0700

    add bugs dir

---
+++ bugs/csv/test.py
@@ -0,0 +1,6 @@
+import rootpy.io
+from rootpy.tree import Tree
+
+f = rootpy.io.open('ttree.root')
+tree = f.get('ParTree_Postselect', ignore_unsupported=True)
+tree.csv()#stream=open('test.csv', 'w'))
