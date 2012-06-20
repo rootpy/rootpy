f6cffe1387df2f2b48575312cdf505f236d5b7b6
Peter Waller <peter.waller@gmail.com>
2012-06-20 19:11:39 +0100

    rootpy/interactive: Fix rootwait crashes once and for all. Add canvas_events.py
    rootwait crashes were fixed by stopping the GUI thread and restarting
    it when we are done.
    
    In addition, also made it possible to optionally close canvases by
    middle clicking them or pressing escape when they have focus (ROOT requires
    the mouse to be over the pad area for this to work). This is provided in
    the canvas_events module.

--- rootpy/interactive/rootwait.py
+++ rootpy/interactive/rootwait.py
@@ -1,63 +1,145 @@
 """
-Keeps root alive until ctrl-c is pressed or all canvases are closed
+The functions in this module provide a way of pausing code execution until
+canvases are closed. This can be useful when testing code and you don't want to
+keep the objects alive outside of your function.
+
+The wait function can be called repeatedly to pause multiple times.
+
+wait_for_zero_canvases()
+    Keeps root alive until CTRL-c is pressed or all canvases are closed
+
+wait_for_zero_canvases(middle_mouse_close=True)
+    allows canvases to be closed with the middle mouse button (see below)
+
+wait is shorthand for wait_for_zero_canvases
 """
+
+import threading
+from pkg_resources import resource_filename
+
+from rootpy.interactive.canvas_events import attach_event_handler
 
 import ROOT as R
 
-exiting = False
+_processRootEvents = getattr(R.PyGUIThread, "_Thread__target", None)
+_finishSchedule = getattr(R.PyGUIThread, "finishSchedule", None)
+
+def start_new_gui_thread():
+    """
+    Attempt to start a new GUI thread, if possible.
+
+    It is only possible to start one if there was one running on module import.
+    """
+    assert not R.PyGUIThread.isAlive(), "GUI thread already running!"
+    
+    assert _processRootEvents, (
+        "GUI thread wasn't started when rootwait was imported, "
+        "so it can't be restarted")
+        
+    R.keeppolling = 1
+    R.PyGUIThread = threading.Thread(None, _processRootEvents, None, (R,))
+    
+    R.PyGUIThread.finishSchedule = _finishSchedule
+    R.PyGUIThread.setDaemon( 1 )
+    R.PyGUIThread.start()
+    
+def stop_gui_thread():
+    """
+    Try to stop the GUI thread. If it was running returns True, otherwise False.
+    """
+    if not R.PyGUIThread.isAlive():
+        return False
+    
+    R.keeppolling = 0
+    R.PyGUIThread.finishSchedule()
+    R.PyGUIThread.join()
+    return True
 
 @R.TPyDispatcher
 def count_canvases():
-    'incpy.ignore'
-    global exiting
-    if not exiting and not get_visible_canvases():
-        exiting = True
+    """
+    Count the number of active canvases and finish gApplication.Run() if there
+    are none remaining.
+    
+    incpy.ignore
+    """
+    if not get_visible_canvases():
         R.gSystem.ExitLoop()
         
 @R.TPyDispatcher
-def terminate():
-    'incpy.ignore'
-    global exiting
-    exiting = True
+def exit_application_loop():
+    """
+    Signal handler for CTRL-c to cause gApplication.Run() to finish.
+    
+    incpy.ignore
+    """    
     print " caught, will stop waiting"
     R.gSystem.ExitLoop()
 
 def get_visible_canvases():
-    'incpy.ignore'
+    """
+    Return a list of active GUI canvases
+    (as opposed to invisible Batch canvases)
+    """
     return [c for c in R.gROOT.GetListOfCanvases() if not c.IsBatch()]
 
-def wait_for_zero_canvases():
-    "Wait for all canvases to be closed, or ctrl-c incpy.ignore"
+def run_application_until_done():
+    print "There are canvases open. Waiting until exit."
+    had_gui_thread = stop_gui_thread()
     
-    global exiting
-    exiting = False
+    R.gApplication._threaded = True
+    R.gApplication.Run(True)
     
-    # Handle ctrl-c
+    if had_gui_thread:
+        start_new_gui_thread()
+
+def wait_for_zero_canvases(middle_mouse_close=False):
+    """
+    Wait for all canvases to be closed, or CTRL-c.
+    
+    If `middle_mouse_close`, middle click will shut the canvas.
+    
+    incpy.ignore
+    """
+        
+    # Handle CTRL-c
     sh = R.TSignalHandler(R.kSigInterrupt, True)
     sh.Add()
-    #sh.Connect("Notified()", "TApplication", R.gApplication, "Terminate()")
-    #sh.Connect("Notified()", "TROOT",        R.gROOT,        "SetInterrupt()")
-    sh.Connect("Notified()", "TPyDispatcher", terminate, "Dispatch()")
+    sh.Connect("Notified()", "TPyDispatcher", exit_application_loop, "Dispatch()")
     
     visible_canvases = get_visible_canvases()
-    
-    global count_canvases
-            
+                
     for canvas in visible_canvases:
+        if middle_mouse_close:
+            attach_event_handler(canvas)
+    
         if not getattr(canvas, "_py_close_dispatcher_attached", False):
+            # Attach a handler only once to each canvas
             canvas._py_close_dispatcher_attached = True
             canvas.Connect("Closed()", "TPyDispatcher",
                            count_canvases, "Dispatch()")
-                           
-        canvas.Update()
-        
+    
     if visible_canvases and not R.gROOT.IsBatch():
-        print "There are canvases open. Waiting until exit."
-        R.gApplication.Run(True)
-        exiting = True
+        run_application_until_done()
 
 wait = wait_for_zero_canvases
 
 def prevent_close_with_canvases():
+    """
+    Register a handler which prevents python from exiting until
+    all canvases are closed
+    """
     register(wait_for_zero_canvases)
 
+def test():
+    c = R.TCanvas()
+    c.Update()
+    wait()
+    
+    c2 = R.TCanvas()
+    c2.Update()
+    wait(True) # This canvas can be killed by middle clicking on it or hitting
+               # escape whilst it has focus
+
+if __name__ == "__main__":
+    test()
