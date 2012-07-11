6529aba8b7b6c70ff34d4b5dc246b136019d375e
Noel Dawe <noel.dawe@cern.ch>
2012-07-11 13:30:47 -0500

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'rootpy'
-copyright = u'2011, Noel Dawe, Jeff Klukas'
+copyright = u'2012, rootpy'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
