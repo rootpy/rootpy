0e7adf041928045687555cedff4bf28042f174d4
Noel Dawe <noel.dawe@gmail.com>
2012-09-04 19:40:13 -0700

    separate out TreeBuffer and TreeModel

---
+++ rootpy/tree/buffer.py
@@ -0,0 +1,228 @@
+import re
+
+from ..types import *
+from .treeobject import *
+from ..registry import lookup_by_name, lookup_demotion
+from ..utils import create
+from ..core import _resetable_mixin, _copy_construct_mixin
+
+
+class TreeBuffer(dict):
+    """
+    A dictionary mapping branch names to values
+    """
+    ARRAY_PATTERN = re.compile('^(?P<type>[^\[]+)\[(?P<length>\d+)\]$')
+
+    def __init__(self, branches=None,
+                       tree=None,
+                       ignore_unsupported=False):
+
+        super(TreeBuffer, self).__init__()
+        self._fixed_names = {}
+        self._branch_cache = {}
+        self._tree = tree
+        self._ignore_unsupported = ignore_unsupported
+        self._current_entry = 0
+        self._collections = {}
+        self._objects = []
+        self.userdata = {}
+        self._entry = Int(0)
+        if branches is not None:
+            self.__process(branches)
+        self._inited = True
+
+    @classmethod
+    def __clean(cls, branchname):
+
+        # Replace invalid characters with '_'
+        branchname = re.sub('[^0-9a-zA-Z_]', '_', branchname)
+        # Remove leading characters until we find a letter or underscore
+        return re.sub('^[^a-zA-Z_]+', '', branchname)
+
+    def __process(self, branches):
+
+        if not branches:
+            return
+        if not isinstance(branches, dict):
+            try:
+                branches = dict(branches)
+            except TypeError:
+                raise TypeError("branches must be a dict or anything "
+                                "the dict constructor accepts")
+
+        methods = dir(self)
+        processed = []
+
+        for name, vtype in branches.items():
+
+            if name in processed:
+                raise ValueError("duplicate branch name %s" % name)
+
+            processed.append(name)
+            obj = None
+
+            array_match = re.match(TreeBuffer.ARRAY_PATTERN, vtype)
+            if array_match:
+                vtype = array_match.group('type') + '[]'
+                length = int(array_match.group('length'))
+                # try to lookup type in registry
+                cls, _ = lookup_by_name(vtype)
+                if cls is not None:
+                    obj = cls(length)
+            else:
+                # try to lookup type in registry
+                cls, inits = lookup_by_name(vtype)
+                if cls is not None:
+                    obj = cls()
+                    for init in inits:
+                        init(obj)
+                else:
+                    # last resort: try to create ROOT.'vtype'
+                    obj = create(vtype)
+            if obj is None:
+                if not self._ignore_unsupported:
+                    raise TypeError("unsupported type "
+                                    "for branch %s: %s" % (name, vtype))
+            else:
+                self[name] = obj
+
+    def __setitem__(self, name, value):
+
+        # for a key to be used as an attr it must be a valid Python identifier
+        fixed_name = TreeBuffer.__clean(name)
+        if fixed_name in dir(self) or fixed_name.startswith('_'):
+            raise ValueError("illegal branch name: %s" % name)
+        if fixed_name != name:
+            self._fixed_names[fixed_name] = name
+        super(TreeBuffer, self).__setitem__(name, value)
+
+    def reset(self):
+
+        # TODO improvements needed here...
+        for value in self.itervalues():
+            if isinstance(value, (Variable, VariableArray, _resetable_mixin)):
+                value.reset()
+            elif isinstance(value, ROOT.ObjectProxy):
+                value.clear()
+            else:
+                value.__init__()
+
+    def flat(self, branches=None):
+
+        flat_branches = []
+        if branches is None:
+            branches = self.keys()
+        for var in branches:
+            demotion = lookup_demotion(self[var].__class__)
+            if demotion is None:
+                raise ValueError(
+                    "branch %s of type %s was not previously registered" % \
+                    (var, self[var].__class__.__name__))
+            flat_branches.append((var, demotion))
+        return TreeBuffer(flat_branches)
+
+    def update(self, branches):
+
+        if isinstance(branches, TreeBuffer):
+            self._entry = branches._entry
+            for name, value in branches.items():
+                super(TreeBuffer, self).__setitem__(name, value)
+            self._fixed_names.update(branches._fixed_names)
+        else:
+            self.__process(branches)
+
+    def set_tree(self, tree=None):
+
+        self._branch_cache = {}
+        self._tree = tree
+        self._current_entry = 0
+
+    def next_entry(self):
+
+        self._branch_cache = {}
+        self._current_entry += 1
+
+    def __setattr__(self, attr, value):
+        """
+        Maps attributes to values.
+        Only if we are initialized
+        """
+        # this test allows attributes to be set in the __init__ method
+        # any normal attributes are handled normally
+        if '_inited' not in self.__dict__ or attr in self.__dict__:
+            return super(TreeBuffer, self).__setattr__(attr, value)
+        elif attr in self:
+            variable = self.__getitem__(attr)
+            if isinstance(variable, (Variable, VariableArray)):
+                variable.set(value)
+                return
+            elif isinstance(variable, _copy_construct_mixin):
+                variable.set_from(value)
+                return
+            raise TypeError("cannot set "
+                            "attribute '%s' of %s instance" % \
+                            (attr, self.__class__.__name__))
+        raise AttributeError("%s instance has no attribute '%s'" % \
+                             (self.__class__.__name__, attr))
+
+    def __getattr__(self, attr):
+
+        if '_inited' not in self.__dict__:
+            raise AttributeError("%s instance has no attribute '%s'" % \
+                                 (self.__class__.__name__, attr))
+        if attr in self._fixed_names:
+            attr = self._fixed_names[attr]
+        try:
+            if self._tree is not None:
+                if attr not in self._branch_cache:
+                    # attr branch is being accessed for the first time in this
+                    # event
+                    branch = self._tree.GetBranch(attr)
+                    if not branch:
+                        raise AttributeError
+                    self._branch_cache[attr] = branch
+                    branch.GetEntry(self._current_entry)
+            variable = self[attr]
+            if isinstance(variable, Variable):
+                return variable.value
+            return variable
+        except (KeyError, AttributeError):
+            raise AttributeError("%s instance has no attribute '%s'" % \
+                                 (self.__class__.__name__, attr))
+
+    def reset_collections(self):
+
+        for coll in self._collections.iterkeys():
+            coll.reset()
+
+    def define_collection(self, name, prefix, size, mix=None):
+
+        coll = TreeCollection(self, name, prefix, size, mix=mix)
+        object.__setattr__(self, name, coll)
+        self._collections[coll] = (name, prefix, size, mix)
+
+    def define_object(self, name, prefix, mix=None):
+
+        cls = TreeObject
+        if mix is not None:
+            cls = mix_treeobject(mix)
+        object.__setattr__(self, name, TreeObject(self, name, prefix))
+        self._objects.append((name, prefix, mix))
+
+    def set_objects(self, other):
+
+        for args in other._objects:
+            self.define_object(*args)
+        for args in other._collections.itervalues():
+            self.define_collection(*args)
+
+    def __str__(self):
+
+        return self.__repr__()
+
+    def __repr__(self):
+
+        rep = ""
+        for name, value in self.items():
+            rep += "%s ==> %s\n" % (name, value)
+        return rep
---
+++ rootpy/tree/model.py
@@ -0,0 +1,154 @@
+import inspect
+from cStringIO import StringIO
+import types
+from ROOT import gROOT
+
+from ..types import *
+from .buffer import TreeBuffer
+
+
+class TreeModelMeta(type):
+    """
+    Metaclass for all TreeModels
+    Addition/subtraction of TreeModels is handled
+    as set union and difference of class attributes
+    """
+    def __new__(cls, name, bases, dct):
+
+        for attr, value in dct.items():
+            TreeModelMeta.checkattr(attr, value)
+        return type.__new__(cls, name, bases, dct)
+
+    def __add__(cls, other):
+
+        return type('_'.join([cls.__name__, other.__name__]),
+                    (cls, other), {})
+
+    def __iadd__(cls, other):
+
+        return cls.__add__(other)
+
+    def __sub__(cls, other):
+
+        attrs = dict(set(cls.get_attrs()).difference(set(other.get_attrs())))
+        return type('_'.join([cls.__name__, other.__name__]),
+                    (TreeModel,), attrs)
+
+    def __isub__(cls, other):
+
+        return cls.__sub__(other)
+
+    def __setattr__(cls, attr, value):
+
+        TreeModelMeta.checkattr(attr, value)
+        type.__setattr__(cls, attr, value)
+
+    @classmethod
+    def checkattr(metacls, attr, value):
+        """
+        Only allow class attributes that are instances of
+        rootpy.types.Column, ROOT.TObject, or ROOT.ObjectProxy
+        """
+        if not isinstance(value, (types.MethodType,
+                                  types.FunctionType,
+                                  classmethod,
+                                  staticmethod,
+                                  property)):
+            if attr in dir(type('dummy', (object,), {})) + \
+                    ['__metaclass__']:
+                return
+            if attr.startswith('_'):
+                raise SyntaxError("TreeModel attribute '%s' "
+                                  "must not start with '_'" % attr)
+            if not inspect.isclass(value):
+                if not isinstance(value, Column):
+                    raise TypeError("TreeModel attribute '%s' "
+                                    "must be an instance of "
+                                    "rootpy.types.Column" % attr)
+                return
+            if not issubclass(value, (ROOT.TObject, ROOT.ObjectProxy)):
+                raise TypeError("TreeModel attribute '%s' must inherit "
+                                "from ROOT.TObject or ROOT.ObjectProxy" % attr)
+
+    def prefix(cls, name):
+        """
+        Create a new TreeModel where class attribute
+        names are prefixed with name
+        """
+        attrs = dict([(name + attr, value) for attr, value in cls.get_attrs()])
+        return TreeModelMeta('_'.join([name, cls.__name__]),
+                    (TreeModel,), attrs)
+
+    def suffix(cls, name):
+        """
+        Create a new TreeModel where class attribute
+        names are suffixed with name
+        """
+        attrs = dict([(attr + name, value) for attr, value in cls.get_attrs()])
+        return TreeModelMeta('_'.join([cls.__name__, name]),
+                    (TreeModel,), attrs)
+
+    def get_attrs(cls):
+        """
+        Get all class attributes
+        """
+        ignore = dir(type('dummy', (object,), {})) + \
+                 ['__metaclass__']
+        attrs = [item for item in inspect.getmembers(cls)
+                 if item[0] not in ignore
+                 and not isinstance(item[1],
+                     (types.FunctionType,
+                      types.MethodType,
+                      classmethod,
+                      staticmethod,
+                      property))]
+        return attrs
+
+    def to_struct(cls, name=None):
+        """
+        Convert TreeModel into a C struct then compile
+        and import with ROOT
+        """
+        if name is None:
+            name = cls.__name__
+        basic_attrs = dict([(attr_name, value)
+                            for attr_name, value in cls.get_attrs()
+                            if isinstance(value, Column)])
+        if not basic_attrs:
+            return None
+        src = 'struct %s {' % name
+        for attr_name, value in basic_attrs.items():
+            src += '%s %s;' % (value.type.typename, attr_name)
+        src += '};'
+        if gROOT.ProcessLine(src) != 0:
+            return None
+        try:
+            exec 'from ROOT import %s; struct = %s' % (name, name)
+            return struct
+        except:
+            return None
+
+    def __repr__(cls):
+
+        out = StringIO()
+        for name, value in cls.get_attrs():
+            print >> out, '%s -> %s' % (name, value)
+        return out.getvalue()[:-1]
+
+    def __str__(cls):
+
+        return repr(cls)
+
+
+class TreeModel(object):
+
+    __metaclass__ = TreeModelMeta
+
+    def __new__(cls, ignore_unsupported=False):
+        """
+        Return a TreeBuffer for this TreeModel
+        """
+        buffer = TreeBuffer(ignore_unsupported=ignore_unsupported)
+        for name, attr in cls.get_attrs():
+            buffer[name] = attr()
+        return buffer
