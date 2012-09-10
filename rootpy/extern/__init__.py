de3447e01d3da98ec51dd13acd6779d720bf93aa
Christoph Deil <Deil.Christoph@gmail.com>
2012-09-10 02:26:32 +0200

    Move external pure-python packages/modules to rootpy/extern
    (for now multilogging, hep and tabulartext was moved there)

---
+++ rootpy/extern/__init__.py
@@ -0,0 +1,11 @@
+"""
+This packages contains copies of python packages that are bundled with rootpy
+but are external to rootpy, and hence are developed in a separate
+source tree.
+
+The reason to include these packages in rootpy is to have fewer
+dependencies, i.e. fewer things to install for the end user.
+And sometimes there are bugs that only surface with certain
+combinations of packages, having everyone use the same version
+of the external packages reduces this problem.
+"""
