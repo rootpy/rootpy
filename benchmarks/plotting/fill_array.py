2f14ec36031874afcb319d82da48cb827bd18181
Noel Dawe <noel.dawe@gmail.com>
2012-11-04 22:35:58 +0100

    use the timeit module in the benchmark

--- benchmarks/plotting/fill_array.py
+++ benchmarks/plotting/fill_array.py
@@ -7,10 +7,20 @@ print __doc__
 from rootpy.plotting import Hist
 import numpy as np
 import cProfile
-import time
+import timeit
+
 
 h = Hist(1000, -5, 5)
 array = np.random.randn(1E6)
+
+def time_repeat(cmd, repeat=5, number=1):
+    best_time = min(
+            timeit.repeat(cmd,
+                repeat=repeat, number=number,
+                setup="from __main__ import h, array, np")) / number
+    print "%d loops, best of %d: %fs per loop" % (
+            number, repeat, best_time)
+
 
 print "Using Python's map()..."
 cProfile.run('map(h.Fill, array)')
@@ -18,10 +28,7 @@ cProfile.run('map(h.Fill, array)')
 h.Reset()
 
 print "time without profiler overhead:"
-t1 = time.time()
-map(h.Fill, array)
-t2 = time.time()
-print "%f seconds" % (t2 - t1)
+time_repeat('map(h.Fill, array)')
 
 h.Reset()
 
@@ -35,10 +42,7 @@ cProfile.run('np.histogram(array)')
 h.Reset()
 
 print "time without profiler overhead:"
-t1 = time.time()
-np.histogram(array)
-t2 = time.time()
-print "%f seconds" % (t2 - t1)
+time_repeat('np.histogram(array)')
 
 h.Reset()
 
@@ -52,7 +56,4 @@ cProfile.run('h.fill_array(array)')
 h.Reset()
 
 print "time without profiler overhead:"
-t1 = time.time()
-h.fill_array(array)
-t2 = time.time()
-print "%f seconds" % (t2 - t1)
+time_repeat('h.fill_array(array)')
