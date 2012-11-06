d9c2301dc6601dec0abe98793b5e7f82df098951
Noel Dawe <noel.dawe@gmail.com>
2012-11-06 23:48:27 +0100

    begin implementation of new registry mechanism in the main __init__.py

--- rootpy/__init__.py
+++ rootpy/__init__.py
@@ -31,3 +31,44 @@ def rootpy_source_dir():
 _ROOTPY_SOURCE_PATH, _ROOTPY_SOURCE_ABSPATH = rootpy_source_dir()
 del rootpy_source_dir
 
+'''
+All rootpy wrappers are registered below. This dict maps the ROOT class name to
+a 2 or 3-tuple of the path to the rootpy class, the rootpy class name, and
+optionally arguments required by the __new__ method to properly set the base
+class (see the Hist, Hist2D and Hist3D classes).
+
+This way rootpy is "aware" of all classes within the package that wrap ROOT
+classes without needing to import everything up front. This registry is required
+to enable rootpy to "cast" ROOT objects into the rootpy form when extracted from
+a ROOT TFile for example.
+'''
+REGISTRY = {
+    'TTree': ('tree.tree', 'Tree',),
+
+    'TDirectoryFile': ('io.file', 'Directory',),
+
+    'TCanvas': ('plotting.canvas', 'Canvas',),
+    'TPad': ('plotting.canvas', 'Pad',),
+    'TLegend': ('plotting.legend', 'Legend',),
+
+    'TGraphAsymmErrors': ('plotting.graph', 'Graph',),
+    'TGraph2D': ('plotting.graph', 'Graph2D',),
+
+    'TH1C': ('plotting.hist', 'Hist', dict(type='C')),
+    'TH1S': ('plotting.hist', 'Hist', dict(type='S')),
+    'TH1I': ('plotting.hist', 'Hist', dict(type='I')),
+    'TH1F': ('plotting.hist', 'Hist', dict(type='F')),
+    'TH1D': ('plotting.hist', 'Hist', dict(type='D')),
+
+    'TH2C': ('plotting.hist', 'Hist2D', dict(type='C')),
+    'TH2S': ('plotting.hist', 'Hist2D', dict(type='S')),
+    'TH2I': ('plotting.hist', 'Hist2D', dict(type='I')),
+    'TH2F': ('plotting.hist', 'Hist2D', dict(type='F')),
+    'TH2D': ('plotting.hist', 'Hist2D', dict(type='D')),
+
+    'TH3C': ('plotting.hist', 'Hist3D', dict(type='C')),
+    'TH3S': ('plotting.hist', 'Hist3D', dict(type='S')),
+    'TH3I': ('plotting.hist', 'Hist3D', dict(type='I')),
+    'TH3F': ('plotting.hist', 'Hist3D', dict(type='F')),
+    'TH3D': ('plotting.hist', 'Hist3D', dict(type='D')),
+}
