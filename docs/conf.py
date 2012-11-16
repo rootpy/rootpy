d7fc0c80d98fb74f75a84eeba727b15e10579d53
Peter Waller <peter@pwaller.net>
2012-11-16 14:49:58 +0000

    Sphinx: Introduce ipython highlighting

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,8 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.pngmath',
     'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode']
+    'sphinx.ext.viewcode',
+    'ipython_console_highlighting']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
