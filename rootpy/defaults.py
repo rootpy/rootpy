61ea38b6c4f91c8ce53103b8a1ab1cc54ead08c9
Peter Waller <peter@pwaller.net>
2012-11-19 21:56:03 +0000

    defaults: Enable logger.magic.DANGER by default.

--- rootpy/defaults.py
+++ rootpy/defaults.py
@@ -4,6 +4,9 @@ import ROOT
 
 from . import log
 from .logger import set_error_handler, python_logging_error_handler
+from .logger.magic import DANGER
+
+DANGER.enabled = True
 
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 ROOT.TH1.SetDefaultSumw2(True)
