a56853b593d841fecc56d842021c4dc51f05d132
Peter Waller <peter.waller@cern.ch>
2012-11-20 22:14:13 +0000

    Comments/tweaks to the previous commit

--- rootpy/defaults.py
+++ rootpy/defaults.py
@@ -25,13 +25,22 @@ log["/ROOT.TUnixSystem.DispatchSignals"].showstack(min_level=log.ERROR)
 orig_error_handler = set_error_handler(python_logging_error_handler)
 
 def configure_defaults():
+    #log.debug("configure_defaults()")
+    
+    # Need to do it again here.
+    set_error_handler(python_logging_error_handler)
+    
     ROOT.TH1.SetDefaultSumw2(True)
 
     # Cause ROOT to abort (and give a stack trace) if it encounters an error,
     # rather than continuing along blindly.
     ROOT.gErrorAbortLevel = ROOT.kError
     ROOT.gErrorIgnoreLevel = 0
-
+    
+    # TODO(pwaller): idea, `execfile("userdata/initrc.py")` here?
+    #                 note: that wouldn't allow the user to override the default
+    #                       canvas size, for example.
+    
 def rp_module_level_in_stack():
     """
     Returns true if we're during a rootpy import
@@ -46,18 +55,24 @@ def rp_module_level_in_stack():
 # Check in case the horse has already bolted.
 # If initialization has already taken place, we can't wrap it.
 if hasattr(ROOT.__class__, "_ModuleFacade__finalSetup"):
+    # Inject our own wrapper in place of ROOT's finalSetup so that we can
+    # trigger our default options then, and .
     
     finalSetup = ROOT.__class__._ModuleFacade__finalSetup
     @wraps(finalSetup)
     def wrapFinalSetup(*args, **kwargs):
         
         if os.environ.get("ROOTPY_DEBUG", None) and rp_module_level_in_stack():
+            # Check to see if we're at module level anywhere in rootpy.
+            # If so, that's not ideal.
             l = log["bug"]
             l.showstack()
-            l.debug("rootpy import isn't complete. Please report this.")
+            l.debug("finalSetup triggered from rootpy at module-level. "
+                    "Please report this.")
         
+        # if running in the ATLAS environment suppress a known harmless warning
         if os.environ.get("AtlasVersion", None):
-            regex = "^duplicate entry .* for level 0; ignored$"
+            regex = "^duplicate entry .* vectorbool.dll> for level 0; ignored$"
             c = log["/ROOT.TEnvRec.ChangeValue"].ignore(regex)
         else:
             @contextmanager
@@ -67,11 +82,14 @@ if hasattr(ROOT.__class__, "_ModuleFacade__finalSetup"):
             result = finalSetup(*args, **kwargs)
         
         configure_defaults()
+        
         return result
     
     ROOT.__class__._ModuleFacade__finalSetup = wrapFinalSetup
     
-    fix_ipython_startup(finalSetup)
+    if "__IPYTHON__" in __builtins__:
+        # ROOT has a bug causing it to print (Bool_t)1 to the console.
+        fix_ipython_startup(finalSetup)
     
 else:
     configure_defaults()
