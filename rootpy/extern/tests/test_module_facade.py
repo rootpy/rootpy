f7d7e33916e92d3f8c5139f60ae1a447c02ea887
Peter Waller <peter@pwaller.net>
2012-11-22 15:45:28 +0000

    Add module_facade

---
+++ rootpy/extern/tests/test_module_facade.py
@@ -0,0 +1,20 @@
+import rootpy.extern.tests.facade_example as F
+
+def test_module_facade():
+    assert F.hello == "hello"
+    assert F.something == "something"
+    assert F.attach_thing("thing") == "thing"
+    assert F.class_level == "class_level"
+    assert "something" in dir(F)
+    assert F["item"] == "item"
+    assert F.module_level_constant == "MODULE_LEVEL_CONSTANT"
+    assert "module_level_constant" in dir(F)
+    assert F.module_level_function("a") == "a"
+    assert "module_level_function" in dir(F)
+    
+def test_internal_facade():
+    from rootpy.extern.tests.facade_example.internal import hello
+    assert hello == "hello"
+    assert F.internal.hello == "hello"
+    assert dir(hello)
+
