71f6312a60982eb5c61c951fba339c82fa93b03d
Noel Dawe <noel.dawe@gmail.com>
2012-12-04 10:14:17 -0800

    add style example

---
+++ examples/plotting/plot_style.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""
+==========================
+Setting the plotting style
+==========================
+
+This example demonstrates how to set the plotting style.
+"""
+print __doc__
+import ROOT
+import rootpy
+rootpy.log.basic_config_colorized()
+from rootpy.plotting import Hist
+from rootpy.plotting.style import using_style
+from rootpy.interactive import wait
+
+
+with using_style('ATLAS'):
+    hpx = Hist(100, -4, 4, name="hpx", title="This is the px distribution")
+    # generate some random data
+    ROOT.gRandom.SetSeed()
+    for i in xrange(25000):
+        hpx.Fill(ROOT.gRandom.Gaus())
+    hpx.GetXaxis().SetTitle("random variable [unit]")
+    hpx.GetYaxis().SetTitle("#frac{dN}{dr} [unit^{-1}]")
+    hpx.SetMaximum(1000.)
+    hpx.Draw()
+    wait()
--- rootpy/plotting/style/atlas/test.py
+++ rootpy/plotting/style/atlas/test.py
@@ -1,19 +1,18 @@
 import ROOT
 from rootpy.plotting import Hist
-from rootpy.plotting.style import use_style
+from rootpy.plotting.style import using_style
 
 
 def test_atlas():
 
-    with use_style('ATLAS'):
-        # generate some random data
+    with using_style('ATLAS'):
         hpx = Hist(100, -4, 4, name="hpx", title="This is the px distribution")
         ROOT.gRandom.SetSeed()
-        for i in xrange(25000):
+        for i in xrange(1000):
             hpx.Fill(ROOT.gRandom.Gaus())
         hpx.GetXaxis().SetTitle("random variable [unit]")
         hpx.GetYaxis().SetTitle("#frac{dN}{dr} [unit^{-1}]")
-        hpx.SetMaximum(1000.)
+        hpx.SetMaximum(100.)
         hpx.Draw()
 
 if __name__ == "__main__":
