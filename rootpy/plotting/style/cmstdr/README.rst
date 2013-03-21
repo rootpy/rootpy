65549cbd6dea8ea7eda12eab9236825db7396aad
Ruggero Turra <ruggero.turra@cern.ch>
2013-03-21 20:49:57 +0100

    add CMSTDR style

---
+++ rootpy/plotting/style/cmstdr/README.rst
@@ -0,0 +1,1 @@
+http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/UserCode/RootMacros/style-CMSTDR.C?revision=1.3&view=markup
---
+++ rootpy/plotting/style/cmstdr/test.py
@@ -0,0 +1,23 @@
+# Copyright 2012 the rootpy developers
+# distributed under the terms of the GNU General Public License
+import ROOT
+from rootpy.plotting import Hist
+from rootpy.plotting.style import get_style
+
+
+def test_cmstdr():
+
+    style = get_style('CMSTDR')
+    with style:
+        hpx = Hist(100, -4, 4, name="hpx", title="This is the px distribution")
+        ROOT.gRandom.SetSeed()
+        for i in xrange(1000):
+            hpx.Fill(ROOT.gRandom.Gaus())
+        hpx.GetXaxis().SetTitle("random variable [unit]")
+        hpx.GetYaxis().SetTitle("#frac{dN}{dr} [unit^{-1}]")
+        hpx.SetMaximum(100.)
+        hpx.Draw()
+
+if __name__ == "__main__":
+    import nose
+    nose.runmodule()
