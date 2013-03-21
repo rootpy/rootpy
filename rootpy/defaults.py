53d6d420579b59d01f2f53a16ccb29bbff0d974a
Peter Waller <peter.waller@cern.ch>
2013-03-21 15:37:27 +0000

    Add rootpy.interactive.interact() to spawn an (I)Python console

--- rootpy/defaults.py
+++ rootpy/defaults.py
@@ -130,6 +130,8 @@ if hasattr(ROOT.__class__, "_ModuleFacade__finalSetup"):
 
         return result
 
+    wrapFinalSetup._orig_func = finalSetup
+
     ROOT.__class__._ModuleFacade__finalSetup = wrapFinalSetup
 
     if "__IPYTHON__" in __builtins__:
