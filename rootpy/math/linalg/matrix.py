72b156a7ee7366feee60ff391fcca4210880ab8f
Peter Waller <p@pwaller.net>
2013-03-22 13:35:56 +0000

    matrix.py/as_numpy: docstring, remove useless import

--- rootpy/math/linalg/matrix.py
+++ rootpy/math/linalg/matrix.py
@@ -1,8 +1,6 @@
 # Copyright 2012 the rootpy developers
 # distributed under the terms of the GNU General Public License
 
-from itertools import product
-
 import numpy as np
 import ROOT
 
@@ -16,5 +14,9 @@ def Matrix(rows, cols, type='F'):
     raise TypeError("No matrix for type '%s'" % type)
 
 def as_numpy(root_matrix):
+    """
+    Returns the given ``root_matrix`` as a ``numpy.matrix``.
+    """
     cols, rows = root_matrix.GetNcols(), root_matrix.GetNrows()
-    return np.matrix([[root_matrix[i][j] for j in range(cols)] for i in xrange(rows)])
+    return np.matrix([[root_matrix[i][j] for j in range(cols)]
+                      for i in xrange(rows)])
