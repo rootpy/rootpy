9c8339f18bc8603e2933b67fafe1a5d36011e434
Evan Friis <ekfriis@gmail.com>
2013-04-01 04:11:27 -0500

    Preserve current canvs + don't let objects get GC-ed

--- rootpy/plotting/style/cmstdr/labels.py
+++ rootpy/plotting/style/cmstdr/labels.py
@@ -4,7 +4,10 @@
 """
 Add the "CMS Preliminary" and \sqrt{s} blurbs to CMS plots.
 """
+
 import ROOT
+from ....context import preserve_current_canvas
+from ....memory.keepalive import keepalive
 
 
 def CMS_label(text="Preliminary 2012", sqrts=8, pad=None):
@@ -16,21 +19,31 @@ def CMS_label(text="Preliminary 2012", sqrts=8, pad=None):
     """
     if pad is None:
         pad = ROOT.gPad
-    l = ROOT.TLatex()
-    l.SetTextAlign(12)  # left-middle
-    l.SetNDC()
-    left_margin = pad.GetLeftMargin()
-    top_margin = pad.GetTopMargin()
-    ypos = 1 - top_margin / 2.
-    # The text is 90% as tall as the margin it lives in.
-    l.SetTextSize(0.90 * top_margin)
-    l.DrawLatex(left_margin, ypos, "CMS " + text)
 
-    # Draw sqrt(s) label, if desired
-    if sqrts:
-        s = ROOT.TLatex()
-        s.SetTextAlign(32)  # right-middle
-        s.SetNDC()
-        s.SetTextSize(0.90 * top_margin)
-        right_margin = pad.GetRightMargin()
-        s.DrawLatex(1 - right_margin, ypos, "#sqrt{s}=%iTeV" % sqrts)
+    with preserve_current_canvas():
+        pad.cd()
+        l = ROOT.TLatex()
+        l.SetTextAlign(12)  # left-middle
+        l.SetNDC()
+        left_margin = pad.GetLeftMargin()
+        top_margin = pad.GetTopMargin()
+        ypos = 1 - top_margin / 2.
+        # The text is 90% as tall as the margin it lives in.
+        l.SetTextSize(0.90 * top_margin)
+        l.DrawLatex(left_margin, ypos, "CMS " + text)
+        keepalive(pad, l)
+
+        # Draw sqrt(s) label, if desired
+        if sqrts:
+            p = ROOT.TLatex()
+            p.SetTextAlign(32)  # right-middle
+            p.SetNDC()
+            p.SetTextSize(0.90 * top_margin)
+            right_margin = pad.GetRightMargin()
+            p.DrawLatex(1 - right_margin, ypos, "#sqrt{s}=%iTeV" % sqrts)
+            keepalive(pad, p)
+        else:
+            p = None
+        pad.Modified()
+        pad.Update()
+        return p, l
