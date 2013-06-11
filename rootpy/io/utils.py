ce8f4d6756f1271777c77bd5803dc0127a6f14df
Noel Dawe <Noel.Dawe@cern.ch>
2013-06-11 09:03:39 -0700

    file utils and roosh improvements

--- rootpy/io/utils.py
+++ rootpy/io/utils.py
@@ -8,7 +8,8 @@ import ROOT
 from fnmatch import fnmatch
 import itertools
 import os
-from .. import asrootpy
+from .. import asrootpy, gDirectory
+from ..context import preserve_current_directory
 from . import DoesNotExist
 
 
@@ -75,27 +76,26 @@ def splitfile(path):
     return filename, os.path.sep + path
 
 
-def rm(path_to_object, cycle=';*'):
+def rm(path, cycle=';*', dir=None):
     ''' Delete an object in a TDirectory
 
-    You must pass the path string, relative to the CWD
+    You must pass the path string relative to dir, otherwise relative to the
+    CWD if dir is None.
     '''
-    # Save state
-    current_path = ROOT.gDirectory.GetPathStatic()
-    # Get the location of the thing to be deleted
-    dirname = os.path.dirname(path_to_object)
-    objname = os.path.basename(path_to_object)
-    ROOT.gDirectory.cd(dirname)
-    ROOT.gDirectory.Delete(objname + cycle)
-    # Restore state
-    ROOT.gDirectory.cd(current_path)
+    if dir is None:
+        dir = gDirectory()
+    with preserve_current_directory():
+        dirname, objname = os.path.split(os.path.normpath(path))
+        if dirname:
+            dir = dir.Get(dirname)
+        dir.Delete(objname + cycle)
 
 
 # This is not trivial as I can't figure out a way to delete using the same
 # interface as cp (i.e. root objects, not paths)
 #def mv(src, dest_dir, newname=None): pass
 
-def cp(src, dest_dir, newname=None, exclude=None):
+def cp(src, dest_dir, newname=None, exclude=None, dir=None):
     ''' Copy an object into another TDirectory.
 
     [src] or [dest_dir] can either be passed as path strings, or as
@@ -109,79 +109,78 @@ def cp(src, dest_dir, newname=None, exclude=None):
     The copied object can optionally be given a [newname].
 
     '''
-    # Always save/restore the state of gDirectory.  Have to use the string path
-    # otherwise gDirectory will change out from under us
-    current_path = ROOT.gDirectory.GetPathStatic()
+    if dir is None:
+        dir = gDirectory()
+    with preserve_current_directory():
+        if isinstance(src, basestring):
+            src = asrootpy(dir.Get(src))
+        if isinstance(dest_dir, basestring):
+            dest_dir = asrootpy(dir.Get(dest_dir))
+        # Check if the object we are copying is not a directory.  Then this is easy
+        if not isinstance(src, ROOT.TDirectory):
+            if newname is not None:
+                src.SetName(newname)
+            dest_dir.cd()
+            if isinstance(src, ROOT.TTree):
+                new_src = src.CloneTree(-1, "fast")
+                new_src.Write()
+            else:
+                src.Write()
+        else:  # We need to copy a directory
+            cp_name = src.GetName()
+            if newname is not None:
+                cp_name = newname
+            # See if the directory already exists
+            new_dir = dest_dir.Get(cp_name)
+            if not new_dir:
+                # It doesn't exist, make the new directory in the destination
+                new_dir = dest_dir.mkdir(cp_name)
+            # Copy everything in the src directory to the destination directory
+            for (path, dirnames, objects) in walk(src, maxdepth=1):
+                # Copy all the objects
+                for object_name in itertools.chain(objects, dirnames):
+                    if exclude and exclude(path, object_name):
+                        continue
+                    object = asrootpy(src.Get(object_name))
+                    # Recursively copy the sub-objects into the dest. dir
+                    cp(object, new_dir, dir=dir)
 
-    if isinstance(src, basestring):
-        src = asrootpy(ROOT.gDirectory.Get(src))
-    if isinstance(dest_dir, basestring):
-        dest_dir = asrootpy(ROOT.gDirectory.Get(dest_dir))
 
-    # Check if the object we are copying is not a directory.  Then this is easy
-    if not isinstance(src, ROOT.TDirectory):
-        if newname is not None:
-            src.SetName(newname)
-        dest_dir.cd()
-        src.Write()
-    else:  # We need to copy a directory
-        cp_name = src.GetName()
-        if newname is not None:
-            cp_name = newname
-        # See if the directory already exists
-        new_dir = dest_dir.Get(cp_name)
-        if not new_dir:
-            # It doesn't exist, make the new directory in the destination
-            new_dir = dest_dir.mkdir(cp_name)
-        # Copy everything in the src directory to the destination directory
-        for (path, dirnames, objects) in walk(src, maxdepth=1):
-            # Copy all the objects
-            for object_name in itertools.chain(objects, dirnames):
-                if exclude and exclude(path, object_name):
-                    continue
-                object = asrootpy(src.Get(object_name))
-                # Recursively copy the sub-objects into the dest. dir
-                cp(object, new_dir)
-    # Restore the state when done
-    ROOT.gDirectory.cd(current_path)
-
-
-def mkdir(dest_path, recurse=False):
-    ''' Make a new directory relative to the CWD
+def mkdir(dest_path, recurse=False, dir=None):
+    ''' Make a new directory
 
     If recurse is True, create parent directories as required.
 
     Return the newly created TDirectory
     '''
-    # Always save/restore the state of gDirectory.  Have to use the string path
-    # otherwise gDirectory will change out from under us
-    current_path = ROOT.gDirectory.GetPathStatic()
-    head, tail = os.path.split(os.path.normpath(dest_path))
-    if tail == "":
-        raise ValueError("invalid directory name: %s" % dest_path)
-    if recurse:
-        parent_dirs = head.split('/')
-        for parent_dir in parent_dirs:
-            try:
-                dest = ROOT.gDirectory.GetDirectory(parent_dir)
-                if not dest:
-                    raise DoesNotExist
-                dest.cd()
-            except DoesNotExist:
-                dest = ROOT.gDirectory.mkdir(parent_dir)
-                dest.cd()
-    elif head != "":
-        dest = ROOT.gDirectory.GetDirectory(head)
-        if not dest:
-            raise DoesNotExist(head)
-        dest.cd()
-    try:
-        dest = ROOT.gDirectory.GetDirectory(tail)
-        if dest:
-            raise ValueError("%s already exists" % dest_path)
-    except DoesNotExist:
-        pass
-    dest = ROOT.gDirectory.mkdir(tail)
-    # Restore the state when done
-    ROOT.gDirectory.cd(current_path)
+    if dir is None:
+        dir = gDirectory()
+    with preserve_current_directory():
+        head, tail = os.path.split(os.path.normpath(dest_path))
+        dest = dir
+        if tail == "":
+            raise ValueError("invalid directory name: %s" % dest_path)
+        if recurse:
+            parent_dirs = head.split('/')
+            for parent_dir in parent_dirs:
+                try:
+                    newdest = dest.GetDirectory(parent_dir)
+                    if not newdest:
+                        raise DoesNotExist
+                    newdest.cd()
+                    dest = newdest
+                except DoesNotExist:
+                    dest = dest.mkdir(parent_dir)
+                    dest.cd()
+        elif head != "":
+            dest = dest.GetDirectory(head)
+            if not dest:
+                raise DoesNotExist(head)
+            dest.cd()
+        try:
+            if dest.GetDirectory(tail):
+                raise ValueError("%s already exists" % dest_path)
+        except DoesNotExist:
+            pass
+        dest = dest.mkdir(tail)
     return dest
