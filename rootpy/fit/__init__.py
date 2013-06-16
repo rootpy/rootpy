375a8a870360dfb2d101464ba0560cd6f0518b42
Noel Dawe <noel.dawe@gmail.com>
2013-06-16 01:34:51 -0700

    grammar

--- rootpy/fit/__init__.py
+++ rootpy/fit/__init__.py
@@ -2,7 +2,7 @@
 # distributed under the terms of the GNU General Public License
 from .. import log; log = log[__name__]
 
-# suppress RooStats info messaged by default
+# suppress RooStats' rather verbose INFO messages by default
 from .. import QROOT
 QROOT.RooMsgService.instance().setGlobalKillBelow(QROOT.RooFit.WARNING)
 
