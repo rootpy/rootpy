fd1e7e92b3a4c53fc9ce8006731950b00564ce80
Noel Dawe <noel.dawe@gmail.com>
2013-06-15 19:39:16 -0700

    fix (#336) failing legend test by creating an invisible canvas

--- rootpy/plotting/tests/test_legend.py
+++ rootpy/plotting/tests/test_legend.py
@@ -2,15 +2,17 @@
 # distributed under the terms of the GNU General Public License
 from ROOT import TH1D
 from rootpy.plotting import Hist, Legend
+from rootpy.context import invisible_canvas
 
 
 def test_init():
 
-    l = Legend(2)
-    h = Hist(10, 0, 1)
-    l.AddEntry(h)
-    hr = TH1D("test", "", 10, 0, 1)
-    l.AddEntry(hr)
+    with invisible_canvas():
+        l = Legend(2)
+        h = Hist(10, 0, 1)
+        l.AddEntry(h)
+        hr = TH1D("test", "", 10, 0, 1)
+        l.AddEntry(hr)
 
 
 if __name__ == "__main__":
