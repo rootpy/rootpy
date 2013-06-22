df3e4a52219b962413cdbb8afd9f05ade9528946
Peter Waller <p@pwaller.net>
2013-06-22 22:47:17 +0100

    Make PaveStats _Positionable

--- rootpy/plotting/box.py
+++ rootpy/plotting/box.py
@@ -1,9 +1,14 @@
 
 from .. import QROOT
 from ..core import Object
+from .utils import canvases_with
 
-class Pave(Object, QROOT.TPave):
-    
+
+# This is another _PadBase hack. See this comment on github
+# https://github.com/rootpy/rootpy/pull/342#issuecomment-19864883
+
+class _Positionable(object):
+
     @property
     def position(self):
         return self.GetX1NDC(), self.GetY1NDC(), self.GetX2NDC(), self.GetY2NDC()
@@ -16,3 +21,11 @@ class Pave(Object, QROOT.TPave):
         self.SetX2NDC(x2)
         self.SetY2NDC(y2)
 
+        for c in canvases_with(self):
+            c.Modified()
+
+class Pave(_Positionable, Object, QROOT.TPave):
+    pass
+
+class PaveStats(_Positionable, Object, QROOT.TPaveStats):
+    pass
