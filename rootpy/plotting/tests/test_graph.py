d9700753f1e6c4f2ca2bec19831be7af2ecd0f9c
Noel Dawe <noel.dawe@gmail.com>
2013-08-01 22:41:35 -0700

    graph unit tests

--- rootpy/plotting/tests/test_graph.py
+++ rootpy/plotting/tests/test_graph.py
@@ -1,15 +1,45 @@
 # Copyright 2012 the rootpy developers
 # distributed under the terms of the GNU General Public License
-from rootpy.plotting import Graph, Graph2D
-from nose.tools import assert_equals
+from rootpy.plotting import Graph, Graph2D, Hist
+import tempfile
+from random import random
+from nose.tools import assert_equal
 
 
 def test_init():
 
     g = Graph(10, name='test')
-    assert_equals(len(g), 10)
-
+    assert_equal(len(g), 10)
     g2d = Graph2D(10, name='test2d')
+
+
+def test_init_from_hist():
+
+    h = Hist(100, -10, 10)
+    h.FillRandom('gaus')
+    g = Graph(h)
+
+
+def test_init_from_file_1d():
+
+    with tempfile.NamedTemporaryFile() as f:
+        for i in xrange(100):
+            f.write('{0:.3f},{1:.3f}\n'.format(
+                random(), random()))
+        f.flush()
+        g = Graph.from_file(f.name, sep=',')
+        assert_equal(len(g), 100)
+
+
+def test_init_from_file_2d():
+
+    with tempfile.NamedTemporaryFile() as f:
+        for i in xrange(100):
+            f.write('{0:.3f},{1:.3f},{2:.3f}\n'.format(
+                random(), random(), random()))
+        f.flush()
+        g = Graph2D.from_file(f.name, sep=',')
+        assert_equal(len(g), 100)
 
 
 if __name__ == "__main__":
