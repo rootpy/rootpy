f02d7eb349075646fa3fc89ca8e67902983fb3aa
Noel Dawe <Noel.Dawe@cern.ch>
2013-08-26 00:35:50 -0700

    plotting.utils._limits_helper: raise ValueError instead of assert and handle case where x1 == x2

---
+++ rootpy/plotting/tests/test_utils.py
@@ -0,0 +1,22 @@
+# Copyright 2012 the rootpy developers
+# distributed under the terms of the GNU General Public License
+from rootpy.plotting.utils import _limits_helper
+from nose.tools import assert_equal, assert_raises
+
+
+def test_limits():
+
+    assert_equal(_limits_helper(0, 1, 0, 0), (0, 1))
+    assert_equal(_limits_helper(1, 1, 0, 0, snap=True), (0, 1))
+    assert_equal(_limits_helper(-2, -1, 0, 0, snap=True), (-2, 0))
+    assert_equal(_limits_helper(-1, 1, .1, .1, snap=True), (-1.25, 1.25))
+    assert_raises(ValueError, _limits_helper, 2, 1, .2, .2)
+    assert_raises(ValueError, _limits_helper, 0, 1, .5, .5)
+    assert_raises(ValueError, _limits_helper, 1, 1, 0, 0)
+    assert_raises(ValueError, _limits_helper, 0, 0, 0, 0)
+    assert_raises(ValueError, _limits_helper, 0, 0, 0, 0, snap=True)
+
+
+if __name__ == "__main__":
+    import nose
+    nose.runmodule()
