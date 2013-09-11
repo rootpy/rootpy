f1ecb9267537c3bebec20e243c5f314e1dc3d73c
Noel Dawe <noel.dawe@gmail.com>
2013-09-11 12:45:25 -0700

    test writing workspace to tempfile: bug in 5.34/09 https://sft.its.cern.ch/jira/browse/ROOT-5502#comment-28047

--- rootpy/stats/histfactory/tests/test_histfactory.py
+++ rootpy/stats/histfactory/tests/test_histfactory.py
@@ -1,5 +1,6 @@
 # Copyright 2012 the rootpy developers
 # distributed under the terms of the GNU General Public License
+from rootpy.io import TemporaryFile
 from rootpy.plotting import Hist
 from rootpy.decorators import requires_ROOT
 from rootpy.stats import mute_roostats; mute_roostats()
@@ -65,6 +66,8 @@ def test_histfactory():
 
     # create the workspace containing the model
     workspace = make_model(meas, silence=True)
+    with TemporaryFile():
+        workspace.Write()
 
     # fit the model to the data
     #obs_data = workspace.data('obsData')
