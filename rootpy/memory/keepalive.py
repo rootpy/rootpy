14017dea9da7a67877d88a74c8c336b3d156dc8e
Noel Dawe <Noel.Dawe@cern.ch>
2014-03-13 17:18:18 -0700

    disable keepalive by setting the env variable NO_ROOTPY_KEEPALIVE

--- rootpy/memory/keepalive.py
+++ rootpy/memory/keepalive.py
@@ -3,6 +3,7 @@
 from __future__ import absolute_import
 
 import weakref
+import os
 
 from . import log; log = log[__name__]
 
@@ -11,6 +12,7 @@ __all__ = [
 ]
 
 KEEPALIVE = weakref.WeakKeyDictionary()
+DISABLED = 'NO_ROOTPY_KEEPALIVE' in os.environ
 
 
 def keepalive(nurse, *patients):
@@ -18,6 +20,8 @@ def keepalive(nurse, *patients):
     Keep ``patients`` alive at least as long as ``nurse`` is around using a
     ``WeakKeyDictionary``.
     """
+    if DISABLED:
+        return
     for p in patients:
-        log.debug("Keeping {0} alive for {1} lifetime".format(p, nurse))
+        log.debug("Keeping {0} alive for lifetime of {1}".format(p, nurse))
     KEEPALIVE.setdefault(nurse, set()).update(patients)
