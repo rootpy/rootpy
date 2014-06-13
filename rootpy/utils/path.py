339d3f1fbd79a75e172e71d58a02e6810fd24d2e
Noel Dawe <noel.dawe@gmail.com>
2014-06-12 23:44:27 -0700

    clean up

--- rootpy/utils/path.py
+++ rootpy/utils/path.py
@@ -15,17 +15,14 @@ __all__ = [
 
 
 def expand(s):
-
     return os.path.expanduser(os.path.expandvars(s))
 
 
 def expand_and_glob(s):
-
     return glob.glob(expand(s))
 
 
 def expand_and_glob_all(s):
-
     files = []
     for name in s:
         files += expand_and_glob(name)
