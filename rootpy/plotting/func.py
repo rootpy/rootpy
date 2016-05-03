f308d3f75e6ff6ff37dda8654d4424a3ee53e00e
mauro verzetti <mverzett@cern.ch>
2016-05-03 10:52:16 -0500

    added BaseFunction class with itemgetter functionality to retrieve parameters provided with a ParProxy sub-class similar to BinProxy

--- rootpy/plotting/func.py
+++ rootpy/plotting/func.py
@@ -14,9 +14,65 @@ __all__ = [
     'F3',
 ]
 
+class BaseFunction(object):
+    class ParProxy(object):
+        def __init__(self, fcn, idx):
+            self.fcn_ = fcn
+            self.idx_ = idx
+        
+        @property
+        def index(self):
+            return self.idx_
+
+        @property
+        def name(self):
+            return self.fcn_.GetParName(self.idx_)
+        
+        @name.setter
+        def set_name(self, val):
+            return self.fcn_.SetParName(self.idx_, val)
+
+        @property
+        def value(self):
+            return self.fcn_.GetParameter(self.idx_)
+
+        @value.setter
+        def set_value(self, val):
+            self.fcn_.SetParameter(self.idx_, val)
+
+        @property
+        def error(self):
+            return self.fcn_.GetParError(self.idx_)
+
+        @error.setter
+        def set_error(self, val):
+            return self.fcn_.SetParError(self.idx_, val)
+
+        @property
+        def limits(self):
+            m = QROOT.Double()
+            M = QROOT.Double()
+            self.fcn_.GetParLimits(self.idx_, m, M)
+            return float(m), float(M)
+
+        @limits.setter
+        def limits(self, val):
+            if not hastattr(val, '__len__') and len(val) != 2:
+                raise RuntimeError('Function limits must be a tuple size 2')
+            self.fcn_.SetParLimits(self.idx_, val[0], val[1])
+
+    def __getitem__(self, value):
+        if isinstance(value, basestring):
+            idx = self.GetParNumber(value)
+        elif isinstance(value, int):
+            idx = value
+        else:
+            raise ValueError('Function index must be a integer or a string')
+        return BaseFunction.ParProxy(self, idx)
+
 
 @snake_case_methods
-class F1(Plottable, NameOnlyObject, QROOT.TF1):
+class F1(Plottable, NameOnlyObject, BaseFunction, QROOT.TF1):
     _ROOT = QROOT.TF1
 
     def __init__(self, *args, **kwargs):
@@ -26,7 +82,7 @@ class F1(Plottable, NameOnlyObject, QROOT.TF1):
 
 
 @snake_case_methods
-class F2(Plottable, NameOnlyObject, QROOT.TF2):
+class F2(Plottable, NameOnlyObject, BaseFunction, QROOT.TF2):
     _ROOT = QROOT.TF2
 
     def __init__(self, *args, **kwargs):
@@ -36,7 +92,7 @@ class F2(Plottable, NameOnlyObject, QROOT.TF2):
 
 
 @snake_case_methods
-class F3(Plottable, NameOnlyObject, QROOT.TF3):
+class F3(Plottable, NameOnlyObject, BaseFunction, QROOT.TF3):
     _ROOT = QROOT.TF3
 
     def __init__(self, *args, **kwargs):
