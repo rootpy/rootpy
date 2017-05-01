dcab1775a021c0cc1912a83e28aa6d5962cfe839
Ben Krikler <b.krikler@cern.ch>
2017-05-01 17:57:33 +0200

    Fix pep8 for unit test

--- rootpy/utils/tests/test_ext_glob.py
+++ rootpy/utils/tests/test_ext_glob.py
@@ -1,28 +1,28 @@
 import unittest
 from rootpy.utils.ext_glob import glob as ext_glob
 from glob import glob as py_glob
-import os,sys
+import os
 
 
-this_directory=os.path.dirname(os.path.abspath(__file__))
+this_directory = os.path.dirname(os.path.abspath(__file__))
 
 
 class TestRootGlob(unittest.TestCase):
 
     def test_local_glob_none(self):
-        filename = os.path.join(this_directory,"test_ext_glob.py")
+        filename = os.path.join(this_directory, "test_ext_glob.py")
         self.assertEqual([filename], ext_glob(filename))
 
     def test_local_glob_basename(self):
-        filename = os.path.join(this_directory,"*.py")
+        filename = os.path.join(this_directory, "*.py")
         self.assertEqual(ext_glob(filename), py_glob(filename))
 
     def test_local_glob_dirname(self):
-        filename = os.path.join(os.path.dirname(this_directory),"*")
+        filename = os.path.join(os.path.dirname(this_directory), "*")
         self.assertEqual(ext_glob(filename), py_glob(filename))
 
     def test_local_glob_both(self):
-        filename = os.path.join(os.path.dirname(this_directory),"*","*.py")
+        filename = os.path.join(os.path.dirname(this_directory), "*", "*.py")
         self.assertEqual(ext_glob(filename), py_glob(filename))
 
 #    def test_xrootd_none(self):
