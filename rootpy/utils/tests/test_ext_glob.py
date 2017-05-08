78efd43db1f0e745cac459499e034ba065d064a0
Ben Krikler <b.krikler@cern.ch>
2017-05-08 15:50:32 +0200

    Rewrite the unit tests for feedback from PR

--- rootpy/utils/tests/test_ext_glob.py
+++ rootpy/utils/tests/test_ext_glob.py
@@ -1,47 +1,44 @@
-import unittest
-from rootpy.utils.ext_glob import glob as ext_glob
-from glob import glob as py_glob
 import os
+from glob import glob as py_glob
+from nose.tools import assert_equal, assert_true
+from nose.plugins.attrib import attr
+from rootpy.utils.ext_glob import glob as ext_glob
 
 
+# It's conceivable that these parameters might be worth overloading / changing
+# from a config file or command line option, particulary the remote_directory
 this_directory = os.path.dirname(os.path.abspath(__file__))
+remote_directory = "root://eospublic.cern.ch//eos/report/eospublic/2015/11"
 
 
-class TestRootGlob(unittest.TestCase):
+def test_local_glob_none():
+    filename = os.path.join(this_directory, "test_ext_glob.py")
+    assert_equal([filename], ext_glob(filename))
 
-    def test_local_glob_none(self):
-        filename = os.path.join(this_directory, "test_ext_glob.py")
-        self.assertEqual([filename], ext_glob(filename))
 
-    def test_local_glob_basename(self):
-        filename = os.path.join(this_directory, "*.py")
-        self.assertEqual(ext_glob(filename), py_glob(filename))
+def test_local_glob_basename():
+    filename = os.path.join(this_directory, "*.py")
+    assert_equal(ext_glob(filename), py_glob(filename))
 
-    def test_local_glob_dirname(self):
-        filename = os.path.join(os.path.dirname(this_directory), "*")
-        self.assertEqual(ext_glob(filename), py_glob(filename))
 
-    def test_local_glob_both(self):
-        filename = os.path.join(os.path.dirname(this_directory), "*", "*.py")
-        self.assertEqual(ext_glob(filename), py_glob(filename))
+def test_local_glob_dirname():
+    filename = os.path.join(os.path.dirname(this_directory), "*")
+    assert_equal(ext_glob(filename), py_glob(filename))
 
-#    def test_xrootd_none(self):
-#       filename = """root://eoscms.cern.ch//eos/cms/store/group/"""
-#       """dpg_trigger/comm_trigger/L1Trigger/L1Menu2016/Stage2/"""
-#       """l1t-integration-v88p1-CMSSW-8021/SingleMuon/"""
-#       """crab_l1t-integration-v88p1-CMSSW-8021__SingleMuon_2016H_v2/"""
-#       """161031_120512/0000/L1Ntuple_999.root""",
 
-#    def test_xrootd_glob_single(self):
-#       filename = """root://eoscms.cern.ch//eos/cms/store/group/"""
-#       """dpg_trigger/comm_trigger/L1Trigger/L1Menu2016/Stage2/"""
-#       """l1t-integration-v88p1-CMSSW-8021/SingleMuon/"""
-#       """crab_l1t-integration-v88p1-CMSSW-8021__SingleMuon_2016H_v2/"""
-#       """161031_120512/0000/L1Ntuple_99*.root"""
+def test_local_glob_both():
+    filename = os.path.join(os.path.dirname(this_directory), "*", "*.py")
+    assert_equal(ext_glob(filename), py_glob(filename))
 
-#    def test_xrootd_glob_multiple(self):
-#       filename = """root://eoscms.cern.ch//eos/cms/store/group/"""
-#       """dpg_trigger/comm_trigger/L1Trigger/L1Menu2016/Stage2/"""
-#       """l1t-integration-v88p1-CMSSW-8021/*/"""
-#       """crab_l1t-integration-v88p1-CMSSW-8021__SingleMuon_2016H_v2/"""
-#       """161031_1*/0000/L1Ntuple_99*.root"""
+
+@attr('network')
+def test_xrootd_glob_single():
+    filename = "/".join([remote_directory, '*'])
+    assert_true(len(ext_glob(filename)) >= 1)
+
+
+@attr('network')
+def test_xrootd_glob_multiple():
+    filename = os.path.dirname(remote_directory)
+    filename = "/".join([filename, '*', '*'])
+    assert_true(len(ext_glob(filename)) >= 2)
