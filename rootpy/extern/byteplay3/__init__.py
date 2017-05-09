e759fd0963be7c6726605b6770f5561140da4933
Noel Dawe <noel.dawe@gmail.com>
2017-05-09 11:13:51 +1000

    byteplay: fix import

--- rootpy/extern/byteplay3/__init__.py
+++ rootpy/extern/byteplay3/__init__.py
@@ -4,6 +4,6 @@ if version_info.major < 3:
     raise ImportError("byteplay3 requires python 3")
 
 if version_info.minor < 6:
-    from byteplay import *
+    from .byteplay import *
 else:
-    from wbyteplay import *
+    from .wbyteplay import *
