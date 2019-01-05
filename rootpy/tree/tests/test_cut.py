20110ea72df3f039882f285d5dd16b43c51f9865
Bakar Chargeishvili <bakar.chargeishvili@gmx.de>
2019-01-05 14:53:07 +0400

    Unit test for ternary cuts

--- rootpy/tree/tests/test_cut.py
+++ rootpy/tree/tests/test_cut.py
@@ -8,6 +8,7 @@ def test_safe():
     assert_equal(Cut("var**2").safe(), "var_pow_2")
     assert_equal(Cut("var*2").safe(), "var_mul_2")
     assert_equal(Cut("2*var**2").safe(), "2_mul_var_pow_2")
+    assert_equal(Cut("-4+1<fabs(var)*1e-3<4*10").safe(), "L-4+1_lt_fabsLvarR_mul_1e-3R_and_LfabsLvarR_mul_1e-3_lt_4_mul_10R")
 
 if __name__ == "__main__":
     import nose
